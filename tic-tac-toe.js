d25e0bad82a789910980adf540c67167e21cc158
Rayna Jarrett <raynasydonia13@gmail.com>
2024-10-27 16:51:03 -0500

    Excersise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const squares = document.querySelectorAll("#board div");
+    const statusDisplay = document.getElementById("status");
     let currentPlayer = "X";
     const gameState = Array(9).fill(null);
 
@@ -19,8 +20,33 @@ document.addEventListener("DOMContentLoaded", () => {
                 gameState[index] = currentPlayer;
                 square.textContent = currentPlayer;
                 square.classList.add(currentPlayer);
-                currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+                if (Winner(currentPlayer)) {
+                    statusDisplay.textContent = `Congratulations! ${currentPlayer} is the Winner!`;
+                    statusDisplay.classList.add("you-won"); 
+                }
+                else {
+                    currentPlayer = currentPlayer === "X" ? "O" : "X";
+                }
+                
             }
         });
     });
+
+    function Winner(player) {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        return winningCombinations.some(combo => {
+            const[a, b, c] = combo;
+            return gameState[a] === player && gameState[b] === player && gameState[c] === player;
+        }); 
+    } 
 });
